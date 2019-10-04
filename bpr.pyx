16bac3b517131c08dc8f5a1d33df1dbe70fd2463
Minato SATO <sato.minato@ohsuga.is.uec.ac.jp>
2019-10-05 00:10:13 +0900

    chaos

--- bpr.pyx
+++ bpr.pyx
@@ -6,19 +6,22 @@
 # LICENSE file in the root directory of this source tree.
 #
 
-import numpy as np
 import cython
+import multiprocessing
+import numpy as np
+from collections import Counter
+from cython.parallel import prange
+from cython.parallel import threadid
+from sklearn import utils
+from tqdm import tqdm
+
+cimport numpy as np
 from cython cimport floating
 from cython cimport integral
-cimport numpy as np
-from libcpp.vector cimport vector
-import multiprocessing
 from libcpp cimport bool
-from sklearn import utils
-from cython.parallel import prange
-from threading import Thread
-from cython.parallel import threadid
-from tqdm import tqdm
+from libcpp.vector cimport vector
+from libcpp.string cimport string
+from libcpp.unordered_map cimport unordered_map
 
 cdef extern from "math.h":
     double exp(double x) nogil
@@ -33,6 +36,9 @@ cdef inline floating sigmoid(floating x) nogil:
 cdef inline floating square(floating x) nogil:
     return x * x
 
+cdef inline string to_cpp_str(str x):
+    return <string> x.encode("utf-8")
+
 class BPR(object):
     def __init__(self, unsigned int num_components,
                        floating learning_rate = 0.01,
@@ -41,44 +47,31 @@ class BPR(object):
         self.learning_rate = learning_rate
         self.weight_decay = weight_decay
 
-    def fit(self, X, 
-                  unsigned int num_iterations,
-                  unsigned int num_threads,
+    def fit(self, X, X_valid = None,
+                  unsigned int num_iterations = 10,
+                  unsigned int num_threads = 8,
                   bool verbose = False):
 
         self.W = np.random.uniform(low=-0.1, high=0.1, size=(X.shape[0], self.num_components)) / self.num_components
         self.H = np.random.uniform(low=-0.1, high=0.1, size=(X.shape[1], self.num_components)) / self.num_components
+        num_threads = min(num_threads, multiprocessing.cpu_count())
 
         users, positives = utils.shuffle(*(X.nonzero()))
         dense = np.array(X.todense())
-        num_threads = min(num_threads, multiprocessing.cpu_count())
-        fit_bpr(users, 
-                positives,
-                dense,
-                self.W,
-                self.H,
-                num_iterations,
-                self.learning_rate,
-                self.weight_decay,
-                num_threads,
-                verbose)
 
-    def fit_partial(self, X, 
-                          unsigned int num_iterations,
-                          unsigned int num_threads,
-                          bool verbose = False):
-
-        if not hasattr(self, "W"):
-            self.W = np.random.uniform(low=-0.1, high=0.1, size=(X.shape[0], self.num_components)) / self.num_components
-        if not hasattr(self, "H"):
-            self.H = np.random.uniform(low=-0.1, high=0.1, size=(X.shape[1], self.num_components)) / self.num_components
-
-        users, positives = (X.nonzero())
-        dense = np.array(X.todense())
-        num_threads = min(num_threads, multiprocessing.cpu_count())
+        users_valid = None
+        positives_valid = None
+        dense_valid = None
+        if X_valid is not None:
+            users_valid, positives_valid = X_valid.nonzero()
+            dense_valid = np.array(X_valid.todense())
+            
         return fit_bpr(users, 
                        positives,
                        dense,
+                       users_valid,
+                       positives_valid,
+                       dense_valid,
                        self.W,
                        self.H,
                        num_iterations,
@@ -87,11 +80,38 @@ class BPR(object):
                        num_threads,
                        verbose)
 
+    # def fit_partial(self, X, 
+    #                       unsigned int num_iterations,
+    #                       unsigned int num_threads,
+    #                       bool verbose = False):
+
+    #     if not hasattr(self, "W"):
+    #         self.W = np.random.uniform(low=-0.1, high=0.1, size=(X.shape[0], self.num_components)) / self.num_components
+    #     if not hasattr(self, "H"):
+    #         self.H = np.random.uniform(low=-0.1, high=0.1, size=(X.shape[1], self.num_components)) / self.num_components
+
+    #     users, positives = X.nonzero()
+    #     dense = np.array(X.todense())
+    #     num_threads = min(num_threads, multiprocessing.cpu_count())
+    #     return fit_bpr(users, 
+    #                    positives,
+    #                    dense,
+    #                    self.W,
+    #                    self.H,
+    #                    num_iterations,
+    #                    self.learning_rate,
+    #                    self.weight_decay,
+    #                    num_threads,
+    #                    verbose)
+
 @cython.boundscheck(False)
 @cython.wraparound(False)
 def fit_bpr(integral[:] users,
             integral[:] positives,
-            np.ndarray[floating, ndim=2] X, 
+            np.ndarray[floating, ndim=2] X,
+            integral[:] users_valid,
+            integral[:] positives_valid,
+            np.ndarray[floating, ndim=2] X_valid,
             floating[:,:] W, 
             floating[:,:] H, 
             unsigned int num_iterations, 
@@ -108,20 +128,33 @@ def fit_bpr(integral[:] users,
     cdef floating[:] l2_norm = np.zeros(N)
     cdef floating[:] gradient_base = np.zeros(N)
     cdef floating[:] loss = np.zeros(N)
-    cdef floating acc_loss
+
+    cdef unordered_map[string, double] metrics
     
     cdef list description_list
 
     cdef integral[:] negative_samples
     cdef integral[:,:] negatives = np.zeros((N, iterations)).astype(np.int32)
+    cdef integral[:,:] negatives_valid = None
+
+    cdef vector[unordered_map[string, double]] history = []
+
     for l in range(N):
         u = users[l]
         negative_samples = np.random.choice((X[u]-1).nonzero()[0], iterations).astype(np.int32)
         negatives[l][:] = negative_samples
 
+    if X_valid is not None:
+        negatives_valid = np.zeros((users_valid.shape[0], iterations)).astype(np.int32)
+        for l in range(users_valid.shape[0]):
+            u = users_valid[l]
+            negative_samples = np.random.choice((X_valid[u]-1).nonzero()[0], iterations).astype(np.int32)
+            negatives_valid[l][:] = negative_samples
+
+
     with tqdm(total=iterations, leave=True, ncols=100, disable=not verbose) as progress:
         for iteration in range(iterations):
-            acc_loss = 0.0
+            metrics[to_cpp_str("loss")] = 0.0
             for l in prange(N, nogil=True, num_threads=num_threads):
                 x_uij[l] = 0.0
                 l2_norm[l] = 0.0
@@ -142,13 +175,33 @@ def fit_bpr(integral[:] users,
                 loss[l] = - log(sigmoid(x_uij[l])) + weight_decay * l2_norm[l]
 
             for l in range(N):
-                acc_loss += loss[l]
+                metrics[to_cpp_str("loss")] += loss[l]
+            metrics[to_cpp_str("loss")] /= N
+
+            if X_valid is not None:
+                metrics[to_cpp_str("val_loss")] = 0.0
+                for l in prange(users_valid.shape[0], nogil=True, num_threads=num_threads):
+                    x_uij[l] = 0.0
+                    l2_norm[l] = 0.0
+                    for k in range(K):
+                        x_uij[l] += W[users_valid[l], k] * (H[positives_valid[l], k] - H[negatives_valid[l][iteration], k])
+                        l2_norm[l] += square(W[users_valid[l], k])
+                        l2_norm[l] += square(H[positives_valid[l], k])
+                        l2_norm[l] += square(H[negatives_valid[l][iteration], k])
+
+                    loss[l] = - log(sigmoid(x_uij[l])) + weight_decay * l2_norm[l]
+                for l in range(users_valid.shape[0]):
+                    metrics[to_cpp_str("val_loss")] += loss[l]
+                metrics[to_cpp_str("val_loss")] /= users_valid.shape[0]
 
             description_list = []
             description_list.append(f"ITER={iteration+1:{len(str(iterations))}}")
-            description_list.append(f"LOSS: {np.round(acc_loss/N, 4):.4f}")
+            description_list.append(f"LOSS: {np.round(metrics[to_cpp_str('loss')], 4):.4f}")
+            if X_valid is not None:
+                description_list.append(f"VAL_LOSS: {np.round(metrics[to_cpp_str('val_loss')], 4):.4f}")
             progress.set_description(', '.join(description_list))
             progress.update(1)
 
-    return acc_loss / N
+            history.push_back(metrics)
+    return history
 
--- main.py
+++ main.py
@@ -18,11 +18,13 @@ from metrics import recall_at_k
 from metrics import dcg_at_k
 
 from tqdm import tqdm
+import pandas as pd
+import matplotlib.pyplot as plt
 
 import argparse
 parser = argparse.ArgumentParser(description='')
 parser.add_argument('--limit', type=int, default=20)
-parser.add_argument('--iter', type=int, default=50)
+parser.add_argument('--iter', type=int, default=200)
 parser.add_argument('--num_components', type=int, default=30)
 parser.add_argument('--lr', type=float, default=0.01)
 parser.add_argument('--weight_decay', type=float, default=0.01)
@@ -33,17 +35,23 @@ args = parser.parse_args()
 dataset: Dataset = MovieLens("ml-100k")
 
 model = BPR(num_components=args.num_components, learning_rate=args.lr, weight_decay=args.weight_decay)
-loss: float
-with tqdm(total=args.iter, ncols=200) as progress:
-    for i in range(args.iter):
-        loss = model.fit_partial(dataset.train, num_iterations=1, num_threads=args.threads, verbose=False)
-        description = f"ITER={i}, LOSS={loss:.4f}, NDCG@10={dcg_at_k(model, dataset.test, k=10).mean():.4f}, "
-        description += f"Recall@10={recall_at_k(model, dataset.test, k=10).mean():.4f}"
-        progress.set_description(description)
-        progress.update(1)
+history = model.fit(dataset.train, dataset.valid, num_iterations=args.iter, num_threads=args.threads, verbose=True)
 
-# print(precision_at_k(model, dataset.test, k=10).mean())
-# print(dcg_at_k(model, dataset.test, k=10).mean())
+
+# loss: float
+# with tqdm(total=args.iter, ncols=200) as progress:
+#     for i in range(args.iter):
+#         loss = model.fit_partial(dataset.train, num_iterations=1, num_threads=args.threads, verbose=False)
+#         description = f"ITER={i}, LOSS={loss:.4f}, NDCG@10={dcg_at_k(model, dataset.test, k=10).mean():.4f}, "
+#         description += f"Recall@10={recall_at_k(model, dataset.test, k=10).mean():.4f}"
+#         progress.set_description(description)
+#         progress.update(1)
+
+print(precision_at_k(model, dataset.test, k=10).mean())
+print(dcg_at_k(model, dataset.test, k=10).mean())
+
+pd.DataFrame(history).plot()
+plt.show()
 
 # model = WMF(num_components=args.num_components, learning_rate=args.lr, weight_decay=args.weight_decay)
 # model.fit(dataset.train, num_iterations=3, num_threads=args.threads, verbose=True)
