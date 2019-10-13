964b49460b6911786ab8f005f057229c0554a68f
Minato SATO <sato.minato@ohsuga.is.uec.ac.jp>
2019-10-13 21:56:23 +0900

    packaging

---
+++ fastmf/__init__.py
@@ -0,0 +1,12 @@
+from .bpr import BPR
+from .wmf import WMF
+from .glove import GloVe
+
+__copyright__    = 'Copyright (C) 2019 Minato Sato'
+__version__      = '1.0.0'
+__license__      = 'MIT'
+__author__       = 'Minato Sato'
+__author_email__ = 'sato.minato@ohsuga.is.uec.ac.jp'
+__url__          = 'http://github.com/satopirka/matrix-factorization'
+
+__all__ = [BPR, WMF, GloVe]
