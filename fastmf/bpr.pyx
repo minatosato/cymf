b0560b71ffcdb85ea244a3f62b50baef712903cd
Minato SATO <sato.minato@ohsuga.is.uec.ac.jp>
2019-10-20 10:19:50 +0900

    update bpr

--- fastmf/bpr.pyx
+++ fastmf/bpr.pyx
@@ -29,6 +29,7 @@ from libcpp.vector cimport vector
 from libcpp.string cimport string
 from libcpp.unordered_map cimport unordered_map
 
+from .model cimport BprModel
 from .optimizer cimport Optimizer
 from .optimizer cimport Adam
 from .metrics import evaluate
@@ -182,63 +183,3 @@ cdef class BPR(object):
         df.index.name = "epoch"
         return df
 
-cdef class BprModel(object):
-    cdef public double[:,:] W
-    cdef public double[:,:] H
-    cdef public double weight_decay
-    cdef public Optimizer optimizer
-    cdef public int num_threads
-    cdef public double[:] x_uij
-
-    def __init__(self, double[:,:] W, double[:,:] H, Optimizer optimizer, double weight_decay, int num_threads):
-        self.W = W
-        self.H = H
-        self.optimizer = optimizer
-        self.weight_decay = weight_decay
-        self.num_threads = num_threads
-        self.x_uij = np.zeros(self.num_threads)
-
-    @cython.boundscheck(False)
-    @cython.wraparound(False)
-    cdef double forward(self, int u, int i, int j) nogil:
-        cdef int thread_id = threadid() # 自分のスレッドidを元に参照すべきdouble[:] x_uijを決定する
-
-        cdef double tmp, loss, l2_norm
-        cdef int K = self.W.shape[1]
-        cdef int k
-
-        self.x_uij[thread_id] = 0.0
-        l2_norm = 0.0
-        for k in range(K):
-            self.x_uij[thread_id] += self.W[u, k] * (self.H[i, k] - self.H[j, k])
-            l2_norm += square(self.W[u, k]) + square(self.H[i, k]) + square(self.H[j, k])
-
-        loss = - log(sigmoid(self.x_uij[thread_id])) + self.weight_decay * l2_norm
-        
-        return loss
-
-    @cython.boundscheck(False)
-    @cython.wraparound(False)
-    cdef void backward(self, int u, int i, int j) nogil:
-        cdef int thread_id = threadid()
-
-        cdef int N = self.W.shape[0]
-        cdef int M = self.H.shape[0]
-        cdef int K = self.W.shape[1]
-        cdef int k
-
-        cdef double grad_wuk
-        cdef double grad_hik
-        cdef double grad_hjk
-        
-        self.x_uij[thread_id] = (1.0 / (1.0 + exp(self.x_uij[thread_id])))
-
-        for k in range(K):
-            grad_wuk = - (self.x_uij[thread_id] * (self.H[i, k] - self.H[j, k]) - self.weight_decay * self.W[u, k])
-            grad_hik = - (self.x_uij[thread_id] *  self.W[u, k] - self.weight_decay * self.H[i, k])
-            grad_hjk = - (self.x_uij[thread_id] * (-self.W[u, k]) - self.weight_decay * self.H[j, k])
-
-            self.optimizer.update_W(u, k, grad_wuk)
-            self.optimizer.update_H(i, k, grad_hik)
-            self.optimizer.update_H(j, k, grad_hjk)
-                   
